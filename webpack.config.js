d2257f691e055621b3c31242f8310b7a97d31a0f
itsreisv <123587174+itsreisv@users.noreply.github.com>
2023-05-15 13:11:03 -0400

    Initialization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  
+  mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
